e06615732b689b008ea8ab2ef8f54c9c6306bc27
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-03-29 12:06:15 +0600

    added crud operation controller

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Put } from '@nestjs/common';
 
 @Controller('songs')
 export class SongsController {
   @Get()
   findAll() {
     return 'All the Songs';
+  }
+
+  @Get(':id')
+  findOne(@Param() params: any) {
+    return `only one song can shown and the song id is ${params.id}`;
+  }
+  @Put(':id')
+  updateOne() {
+    return 'Only one song can update';
+  }
+  @Delete(':id')
+  delete() {
+    return 'Only one song can delete';
   }
 }
